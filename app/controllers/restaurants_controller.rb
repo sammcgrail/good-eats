29a843882c1689216e84412ccf8a0cf3bfbf89a2
sammcgrail <sammcgrail@gmail.com>
2015-06-26 10:10:45 -0400

    seeds and ten restaurs on page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,15 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.limit(10)
+  end
 
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :state,
+      :zip_code, :description, :category)
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create!(name: "Arbys", address: "1 Main St.", city: "Boston", state: "MA", zipcode: "99909", description: "Meats")
+Restaurant.create!(name: "Boston Market", address: "12 Main St.", city: "Boston", state: "MA", zipcode: "99599", description: "Fast food place")
+Restaurant.create!(name: "Chuck Cheese", address: "13 Main St.", city: "Boston", state: "MA", zipcode: "39999", description: "Got a playground")
+Restaurant.create!(name: "Dennys", address: "14 Main St.", city: "Boston", state: "MA", zipcode: "96999", description: "It's a dennys man")
+Restaurant.create!(name: "Eggs House", address: "15 Main St.", city: "Boston", state: "MA", zipcode: "93999")
+Restaurant.create!(name: "French Place", address: "16 Main St.", city: "Boston", state: "MA", zipcode: "95999")
+Restaurant.create!(name: "Gut house", address: "17 Main St.", city: "NYC", state: "NY", zipcode: "99929")
+Restaurant.create!(name: "Hut of waffle", address: "18 Main St.", city: "Chicago", state: "IL", zipcode: "99949")
+Restaurant.create!(name: "Igloo Food INC.", address: "19 Main St.", city: "Las Vegas", state: "NV", zipcode: "59999")
+Restaurant.create!(name: "Arbys 2", address: "20 Main St.", city: "Miami", state: "FL", zipcode: "19999")
